49d3483b9fad5f624d6fc6979b1d5c0f0363c42f
bestenevoy <bestenevoy@outlook.com>
2025-07-24 22:01:00 +0800

    feat(examples): 添加配置管理示例脚本

---
+++ examples/example_config.py
@@ -0,0 +1,69 @@
+from dataclasses_json import config
+from ai4one.config import BaseConfig, field
+from marshmallow import fields, validate
+from typing import Literal, List
+
+
+# --- 使用示例 ---
+class DataConfig(BaseConfig):
+    name: str = "hahahah"
+    folds: List[int] = field(default_factory=lambda: [0, 1, 2, 3, 4])
+
+
+class ModelConfig(BaseConfig):
+    pass
+
+
+class TrainConfig(BaseConfig):
+    device: Literal["auto", "gpu", "cpu"] = field(
+        default="auto",
+        metadata=config(
+            # 我们告诉 dataclasses-json，不要自己去猜这个字段怎么处理，
+            # 直接使用我们提供的 marshmallow 字段。
+            mm_field=fields.String(
+                # 这个字段必须是一个字符串
+                validate=validate.OneOf(
+                    ["auto", "gpu", "cpu"]
+                )  # 并且它的值必须是这三个选项之一
+            )
+        ),
+    )
+
+
+
+class Config(BaseConfig):
+
+    data: DataConfig = field(default_factory=DataConfig)
+    model: ModelConfig = field(default_factory=ModelConfig)
+    train: TrainConfig = field(default_factory=TrainConfig)
+    
+    mode: Literal["train", "test", "predict"] = field(
+        default="train",
+        metadata=config(
+            # 我们告诉 dataclasses-json，不要自己去猜这个字段怎么处理，
+            # 直接使用我们提供的 marshmallow 字段。
+            mm_field=fields.String(
+                # 这个字段必须是一个字符串
+                validate=validate.OneOf(
+                    ["train", "test", "predict"]
+                )  # 并且它的值必须是这三个选项之一
+            )
+        ),
+    )
+
+
+
+if __name__ == "__main__":
+
+    config = Config.argument_parser()
+    config.to_file("./ai4one_config.json")
+
+    cfg = Config.from_file("./ai4one_config.json")
+    print(cfg.data.name)
+    print(cfg.data.folds)
+
+    """
+    uv run examples/example_config.py --device gpu --name ai4one --folds 2 3 --mode test
+    or 
+    python examples/example_config.py --device gpu --name ai4one --folds 2 3 --mode test
+    """
